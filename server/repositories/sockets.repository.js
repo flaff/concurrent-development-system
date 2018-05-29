a1a221d7f8546180e1bc8238ac8242ea683a9b0b
lukaszd94 <lukasz.dudek.94@gmail.com>
2018-05-30 00:44:01 +0200

    new db schema - session

--- server/repositories/sockets.repository.js
+++ server/repositories/sockets.repository.js
@@ -1,3 +1,5 @@
+const Session = require('../models/session.model');
+
 module.exports = function (io) {
     io.sockets.on('connection', function (socket) {
         socket.emit('connectedToSocket', {socket: socket.id});
