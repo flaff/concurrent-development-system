74dec4d6654885d17d2766f8a6e0b8517be46b20
lukaszd94 <lukasz.dudek.94@gmail.com>
2018-06-09 23:01:32 +0200

    fixed auth guard null and replace() bug

--- server/auth.guard.js
+++ server/auth.guard.js
@@ -2,13 +2,17 @@ const jwt = require('jsonwebtoken');
 const config = require('./config/config');
 
 module.exports = function (req, res, next) {
-    let token = req.headers['authorize'].replace('"', '');
-    jwt.verify(token, config.jwtSalt, function(err, user) {
-        if (err) {
-            return res.status(401).json({message: 'Unathorized! Login using properly credentials'});
-        } else {
-            req.user = user; //set the user to req so other routes can use it
-            return next();
-        }
-    });
+    if (req.headers['authorize']) {
+        let token = req.headers['authorize'].replace('"', '');
+        jwt.verify(token, config.jwtSalt, function (err, user) {
+            if (err) {
+                return res.status(401).json({message: 'Unathorized! Login using properly credentials'});
+            } else {
+                req.user = user; //set the user to req so other routes can use it
+                return next();
+            }
+        });
+    } else {
+        return res.status(401).json({message: 'Unathorized! Login using properly credentials'});
+    }
 };
