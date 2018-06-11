bb484b4fce487897b601f64478e1034125a66344
flaff <flaffy90@gmail.com>
2018-06-12 00:22:54 +0200

    color picker

--- src/components/Simulation/Snapshot/index.tsx
+++ src/components/Simulation/Snapshot/index.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
+import * as React from "react";
+import ColorPicker from './ColorPicker';
 
-const styles = require('./styles.scss');
+const styles = require("./styles.scss");
 
-import {setCanvas} from './drawableCanvas.js';
+import {setCanvas, getColor, setColor} from "./drawableCanvas.js";
 
 interface SnapshotProps extends SnapshotPassedProps {
 
@@ -15,14 +16,22 @@ interface SnapshotPassedProps {
 }
 
 interface SnapshotState {
-
+    colorPickerVisible: boolean;
+    currentColor: string;
 }
 
 class Snapshot extends React.Component<SnapshotProps, SnapshotState> {
     constructor(props: SnapshotProps) {
         super(props);
+        this.state = {
+            colorPickerVisible: false,
+            currentColor: getColor()
+        };
+
         this.sendImage = this.sendImage.bind(this);
         this.applyImageToCanvas = this.applyImageToCanvas.bind(this);
+        this.onColorChange = this.onColorChange.bind(this);
+        this.toggleColorPicker = this.toggleColorPicker.bind(this);
     }
 
     componentDidMount() {
@@ -50,7 +59,7 @@ class Snapshot extends React.Component<SnapshotProps, SnapshotState> {
     applyImageToCanvas() {
         const image = new Image(),
             canvas = this.refs.canvas as HTMLCanvasElement,
-            ctx = canvas.getContext('2d');
+            ctx = canvas.getContext("2d");
 
         image.onload = () => {
             if (ctx) {
@@ -64,25 +73,46 @@ class Snapshot extends React.Component<SnapshotProps, SnapshotState> {
 
     sendImage() {
         const canvas = this.refs.canvas as HTMLCanvasElement;
-        this.props.sendMessage(canvas.toDataURL('image/png'));
+        this.props.sendMessage(canvas.toDataURL("image/png"));
         this.props.onClose();
+    }
+
+    onColorChange(color: {hex: string}) {
+        this.setState({
+            ...this.state,
+            currentColor: color.hex
+        });
+        setColor(color.hex);
+    }
+
+    toggleColorPicker() {
+        this.setState({
+            ...this.state,
+            colorPickerVisible: !this.state.colorPickerVisible
+        });
     }
 
     render() {
         return (
             <div className={styles.snapshot}>
                 <div className={styles.modal}>
-                    <div className={styles.canvas} ref={'canvasContainer'}>
-                        <canvas ref={'canvas'} />
+                    <div className={styles.canvas} ref={"canvasContainer"}>
+                        <canvas ref={"canvas"}/>
                     </div>
                     <div>
-                        <button className={'btn btn-success'} onClick={this.sendImage}>Send</button>
-                        <button className={'btn btn-outline-danger'} onClick={this.applyImageToCanvas}>Clear</button>
-                        <button className={'btn btn-outline-secondary'} onClick={this.props.onClose}>Cancel</button>
+                        {this.state.colorPickerVisible &&
+                        <div className={styles.colorPicker}>
+                            <ColorPicker onChange={this.onColorChange} color={this.state.currentColor}/>
+                        </div>}
+                        <div className={styles.colorPickerButton} style={{background: this.state.currentColor}}
+                             onClick={this.toggleColorPicker}/>
+                        <button className={"btn btn-success"} onClick={this.sendImage}>Send</button>
+                        <button className={"btn btn-outline-danger"} onClick={this.applyImageToCanvas}>Clear</button>
+                        <button className={"btn btn-outline-secondary"} onClick={this.props.onClose}>Cancel</button>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
